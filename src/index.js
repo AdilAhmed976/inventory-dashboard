7879ac684ea3e6d0e7a7f52ba0663db47f52d5d1
AdilAhmed976 <adilahmedah976@gmail.com>
2024-05-26 21:02:21 +0530

    core : Removed Unused file & added a basic setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import { ColorModeScript } from '@chakra-ui/react';
+import { ChakraProvider, theme } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 
 const container = document.getElementById('root');
@@ -10,8 +9,9 @@ const root = ReactDOM.createRoot(container);
 
 root.render(
   <StrictMode>
-    <ColorModeScript />
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </StrictMode>
 );
 
@@ -19,8 +19,3 @@ root.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorker.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
