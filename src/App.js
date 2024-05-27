73d5371c48065c9a6fb7552fa9d210b185257155
AdilAhmed976 <adilahmedah976@gmail.com>
2024-05-27 14:17:54 +0530

    feat : toggle theme button added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import AllRoutes from './Pages/AllRoutes';
+import { Box } from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
 
 function App() {
   return (
     <>
+      <Box
+        sx={{
+          position: 'fixed',
+          bottom: 2,
+          right: 2,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '6px',
+        }}
+      >
+        <ColorModeSwitcher sx={{ width: '100%', border: '1px solid black' }} />
+      </Box>
       <AllRoutes />
     </>
   );
